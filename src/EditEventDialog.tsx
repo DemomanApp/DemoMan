1e479de46edf0a426ac5501f4287292dee6690ac
Narcha <42248344+Narcha@users.noreply.github.com>
2021-05-25 17:31:16 +0200

    added event editing

---
+++ src/EditEventDialog.tsx
@@ -0,0 +1,214 @@
+import React from "react";
+
+import Grid from "@material-ui/core/Grid";
+import Button from "@material-ui/core/Button";
+import TextField from "@material-ui/core/TextField";
+import InputAdornment from "@material-ui/core/InputAdornment";
+
+import SmallDialog from "./SmallDialog";
+import EventTableEntry from "./EventTableEntry";
+
+type EditEventDialogProps = {
+  addCallback: (event: EventTableEntry) => void;
+  editCallback: (event: EventTableEntry) => void;
+  deleteCallback: (event: EventTableEntry) => void;
+  ref: React.RefObject<EditEventDialog>;
+};
+
+type EditEventDialogState = {
+  open: boolean;
+  event: EventTableEntry | null;
+  isEditing: boolean;
+  tickError: boolean;
+  valueError: boolean;
+  hasUnsavedChanges: boolean;
+};
+
+export default class EditEventDialog extends React.Component<
+  EditEventDialogProps,
+  EditEventDialogState
+> {
+  constructor(props: EditEventDialogProps) {
+    super(props);
+    this.state = {
+      open: false,
+      event: null,
+      isEditing: true,
+      tickError: false,
+      valueError: false,
+      hasUnsavedChanges: false,
+    };
+  }
+
+  setEvent(value: EventTableEntry) {
+    // spreading creates a shallow copy of the event
+    this.setState({
+      event: { id: value.id, event: { ...value.event } },
+      tickError: false,
+      valueError: false,
+    });
+  }
+
+  setEditing(value: boolean) {
+    this.setState({ isEditing: value, hasUnsavedChanges: !value });
+  }
+
+  open = () => {
+    this.setState({ open: true });
+  };
+
+  save = () => {
+    const { event, isEditing, tickError, valueError } = this.state;
+    const { editCallback, addCallback } = this.props;
+    if (event === null) {
+      return;
+    }
+    if (tickError || valueError) {
+      return;
+    }
+    this.setState({ open: false });
+    if (event !== null) {
+      (isEditing ? editCallback : addCallback)(event);
+    }
+  };
+
+  cancel = () => {
+    this.setState({ open: false });
+  };
+
+  delete = () => {
+    const { event } = this.state;
+    const { deleteCallback } = this.props;
+    if (event === null) {
+      return;
+    }
+    this.setState({ open: false });
+    if (event !== null) {
+      deleteCallback(event);
+    }
+  };
+
+  validateTickInput = (
+    e: React.ChangeEvent<HTMLTextAreaElement | HTMLInputElement>
+  ) => {
+    const { event } = this.state;
+    if (event === null) {
+      return;
+    }
+    event.event.tick = parseInt(e.target.value, 10);
+    this.setState({
+      tickError: Number.isNaN(event.event.tick),
+      event,
+      hasUnsavedChanges: true,
+    });
+  };
+
+  validateValueInput = (
+    e: React.ChangeEvent<HTMLTextAreaElement | HTMLInputElement>
+  ) => {
+    const { event } = this.state;
+    if (event === null) {
+      return;
+    }
+    event.event.value = e.target.value;
+    this.setState({
+      valueError: event.event.value.length === 0,
+      event,
+      hasUnsavedChanges: true,
+    });
+  };
+
+  render() {
+    const { open, event, isEditing, tickError, valueError, hasUnsavedChanges } =
+      this.state;
+
+    if (event === null) {
+      return null;
+    }
+    return (
+      <SmallDialog
+        title={isEditing ? "Edit Event" : "Create Event"}
+        open={open}
+        onClose={this.cancel}
+      >
+        <Grid item container>
+          <Grid
+            item
+            container
+            direction="row"
+            spacing={2}
+            style={{ margin: "8px" }}
+          >
+            <Grid item xs={4}>
+              <TextField
+                required
+                id="input-tick"
+                label="Tick"
+                value={event.event.tick}
+                type="number"
+                onChange={this.validateTickInput}
+                variant="outlined"
+                error={tickError}
+                InputProps={{
+                  startAdornment: (
+                    <InputAdornment position="start">Tick</InputAdornment>
+                  ),
+                }}
+              />
+            </Grid>
+            <Grid item xs={8}>
+              <TextField
+                required
+                id="input-value"
+                label="Value"
+                value={event.event.value}
+                onChange={this.validateValueInput}
+                variant="outlined"
+                error={valueError}
+                fullWidth
+              />
+            </Grid>
+          </Grid>
+          <Grid
+            item
+            container
+            justify="flex-end"
+            spacing={2}
+            style={{ marginTop: "8px" }}
+          >
+            {isEditing && (
+              <Grid item>
+                <Button
+                  variant="contained"
+                  color="secondary"
+                  onClick={this.delete}
+                >
+                  Delete Event
+                </Button>
+              </Grid>
+            )}
+            <Grid item>
+              <Button
+                variant="contained"
+                color="secondary"
+                onClick={this.cancel}
+              >
+                Cancel
+              </Button>
+            </Grid>
+            <Grid item>
+              <Button
+                variant="contained"
+                color="primary"
+                onClick={this.save}
+                disabled={!hasUnsavedChanges}
+              >
+                Save changes
+              </Button>
+            </Grid>
+          </Grid>
+        </Grid>
+      </SmallDialog>
+    );
+  }
+}
--- src/EventTable.tsx
+++ src/EventTable.tsx
@@ -1,133 +1,74 @@
-import React, { PureComponent } from "react";
+import React from "react";
 import DataTable from "react-data-table-component";
 
 import ArrowDownward from "@material-ui/icons/ArrowDownward";
 import IconButton from "@material-ui/core/IconButton";
-import ClearIcon from "@material-ui/icons/Clear";
-import SaveIcon from "@material-ui/icons/Save";
 import AddIcon from "@material-ui/icons/Add";
 import Tooltip from "@material-ui/core/Tooltip";
 
-import { Demo } from "./Demos";
-import DemoEvent from "./DemoEvent";
 import { getPreferredTheme } from "./theme";
+import EventTableEntry from "./EventTableEntry";
 
 const columns = [
   {
     name: "Tick",
-    selector: "tick",
+    selector: "event.tick",
     sortable: true,
     grow: 1,
   },
   {
-    name: "Name",
-    selector: "name",
+    name: "Type",
+    selector: "event.name",
     sortable: true,
-    grow: 2,
+    grow: 1,
   },
   {
     name: "Value",
-    selector: "value",
+    selector: "event.value",
     sortable: true,
     grow: 4,
   },
 ];
 
 type EventTableProps = {
-  demo: Demo;
+  data: EventTableEntry[];
+  editEvent: (event: EventTableEntry) => void;
+  addEvent: () => void;
 };
 
-type EventTableState = {
-  demo: Demo;
-  data: DemoEvent[];
-  unsavedChanges: boolean;
-};
-
-export default class EventTable extends PureComponent<
-  EventTableProps,
-  EventTableState
-> {
-  constructor(props: EventTableProps) {
-    super(props);
-    this.state = {
-      data: [],
-      demo: props.demo,
-      unsavedChanges: false,
-    };
-  }
-
-  componentDidMount() {
-    this.DiscardChanges();
-  }
-
-  DiscardChanges = async () => {
-    const { demo } = this.state;
-    this.setState({
-      data: await demo.events(),
-    });
-  };
-
-  AddEvent = () => {
-    this.setState({
-      unsavedChanges: true,
-    });
-  };
-
-  SaveChanges = () => {
-    this.setState({
-      unsavedChanges: false,
-    });
-  };
-
-  render() {
-    const { data, unsavedChanges } = this.state;
-
-    return (
-      <DataTable
-        title="Events"
-        columns={columns}
-        defaultSortField="tick"
-        defaultSortAsc
-        highlightOnHover
-        noDataComponent={
-          <div style={{ height: "2rem" }}>
-            This demo doesn&apos;t have any events recorded.
-          </div>
-        }
-        actions={
-          <>
-            <Tooltip title="Add event">
-              <IconButton color="default" onClick={this.AddEvent}>
-                <AddIcon />
-              </IconButton>
-            </Tooltip>
-            {unsavedChanges && (
-              <>
-                <Tooltip title="Discard changes">
-                  <IconButton color="primary" onClick={this.DiscardChanges}>
-                    <ClearIcon />
-                  </IconButton>
-                </Tooltip>
-                <Tooltip title="Save changes">
-                  <IconButton color="primary" onClick={this.SaveChanges}>
-                    <SaveIcon />
-                  </IconButton>
-                </Tooltip>
-              </>
-            )}
-          </>
-        }
-        data={data}
-        sortIcon={<ArrowDownward />}
-        fixedHeader
-        // 64px: Page title,
-        // 1px: Divider,
-        // 24px and 5px: padding,
-        // 56px: table title,
-        // 57px: table header.
-        fixedHeaderScrollHeight="calc(100vh - 64px - 1px - 2 * 24px - 2 * 5px - 56px - 57px)"
-        theme={getPreferredTheme()}
-      />
-    );
-  }
+export default function EventTable(props: EventTableProps) {
+  const { data, editEvent, addEvent } = props;
+  return (
+    <DataTable
+      title="Events"
+      columns={columns}
+      defaultSortField="event.tick"
+      defaultSortAsc
+      highlightOnHover
+      noDataComponent={
+        <div style={{ height: "2rem" }}>
+          This demo doesn&apos;t have any events recorded.
+        </div>
+      }
+      actions={
+        <Tooltip title="Add event">
+          <IconButton onClick={addEvent}>
+            <AddIcon />
+          </IconButton>
+        </Tooltip>
+      }
+      data={data}
+      onRowClicked={editEvent}
+      pointerOnHover
+      sortIcon={<ArrowDownward />}
+      fixedHeader
+      // 64px: Page title,
+      // 1px: Divider,
+      // 24px and 5px: padding,
+      // 56px: table title,
+      // 57px: table header.
+      fixedHeaderScrollHeight="calc(100vh - 64px - 1px - 2 * 24px - 2 * 5px - 56px - 57px)"
+      theme={getPreferredTheme()}
+    />
+  );
 }
