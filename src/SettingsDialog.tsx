743b5daa9603aeee6387ff3e8f9cca6142018965
Narcha <42248344+Narcha@users.noreply.github.com>
2021-08-01 13:04:41 +0200

    Added icons, improved menu positioning in settings dialog

--- src/SettingsDialog.tsx
+++ src/SettingsDialog.tsx
@@ -9,6 +9,12 @@ import List from "@material-ui/core/List";
 import ListItem from "@material-ui/core/ListItem";
 import Menu from "@material-ui/core/Menu";
 import ListItemText from "@material-ui/core/ListItemText";
+import ListItemIcon from "@material-ui/core/ListItemIcon";
+import FolderIcon from "@material-ui/icons/Folder";
+import PaletteIcon from "@material-ui/icons/Palette";
+import DarkThemeIcon from "@material-ui/icons/Brightness3";
+import LightThemeIcon from "@material-ui/icons/Brightness7";
+import SystemThemeIcon from "@material-ui/icons/SettingsApplications";
 
 import { GetDemoPath } from "./GetDemoPath";
 import SmallDialog from "./SmallDialog";
@@ -27,6 +33,12 @@ const ThemeNames: { [key: string]: string } = {
   dark: "Dark",
   light: "Light",
   system: "Follow system theme",
+};
+
+const ThemeIcons: { [key: string]: JSX.Element } = {
+  dark: <DarkThemeIcon />,
+  light: <LightThemeIcon />,
+  system: <SystemThemeIcon />,
 };
 
 export default class SettingsDialog extends React.Component<
@@ -120,6 +132,9 @@ export default class SettingsDialog extends React.Component<
                 this.setState({ themePickerAnchor: event.currentTarget });
               }}
             >
+              <ListItemIcon>
+                <PaletteIcon />
+              </ListItemIcon>
               <ListItemText
                 primary="Theme"
                 secondary={ThemeNames[settings.theme]}
@@ -140,6 +155,9 @@ export default class SettingsDialog extends React.Component<
                 }
               }}
             >
+              <ListItemIcon>
+                <FolderIcon />
+              </ListItemIcon>
               <ListItemText
                 primary="Demo path"
                 secondary={settings.demo_path}
@@ -147,6 +165,8 @@ export default class SettingsDialog extends React.Component<
             </ListItem>
           </List>
           <Menu
+            anchorOrigin={{ horizontal: "center", vertical: "center" }}
+            transformOrigin={{ horizontal: "center", vertical: "top" }}
             anchorEl={themePickerAnchor}
             open={themePickerAnchor !== null}
             keepMounted
@@ -160,7 +180,8 @@ export default class SettingsDialog extends React.Component<
                 selected={settings.theme === key}
                 onClick={() => this.selectTheme(key)}
               >
-                {ThemeNames[key]}
+                <ListItemIcon>{ThemeIcons[key]}</ListItemIcon>
+                <ListItemText>{ThemeNames[key]}</ListItemText>
               </MenuItem>
             ))}
           </Menu>
