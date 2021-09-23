49afeb02e7e3f49d2c9cc56c0a05ac51a288fd76
Narcha <42248344+Narcha@users.noreply.github.com>
2021-09-23 21:35:56 +0200

    Rewrote MainView and SelectDemoPathDialog

--- src/SelectDemoPathDialog.tsx
+++ src/SelectDemoPathDialog.tsx
@@ -9,64 +9,39 @@ import DialogTitle from "@material-ui/core/DialogTitle";
 import { GetSetDemoPath } from "./GetDemoPath";
 
 type SelectDemoPathDialogProps = {
-  onComplete: () => void;
-};
-
-type SelectDemoPathDialogState = {
   open: boolean;
   onComplete: () => void;
 };
 
-export default class SelectDemoPathDialog extends React.PureComponent<
-  SelectDemoPathDialogProps,
-  SelectDemoPathDialogState
-> {
-  constructor(props: SelectDemoPathDialogProps) {
-    super(props);
-    this.state = {
-      open: false,
-      onComplete: props.onComplete,
-    };
-  }
+export default function SelectDemoPathDialog(props: SelectDemoPathDialogProps) {
+  const { open, onComplete } = props;
 
-  setOpen(value: boolean) {
-    this.setState({ open: value });
-  }
-
-  handleClose = () => {
-    const { onComplete } = this.state;
-    this.setOpen(false);
-    onComplete();
-  };
-
-  handleChoose = () => {
-    if (GetSetDemoPath()) {
-      this.handleClose();
-    }
-  };
-
-  render() {
-    const { open } = this.state;
-    return (
-      <Dialog
-        open={open}
-        onClose={this.handleClose}
-        disableBackdropClick
-        disableEscapeKeyDown
-      >
-        <DialogTitle>First time setup</DialogTitle>
-        <DialogContent>
-          <DialogContentText>
-            It looks like you&apos;re opening DemoMan for the first time. <br />
-            Please select the the location where your demo files are stored.
-          </DialogContentText>
-        </DialogContent>
-        <DialogActions>
-          <Button onClick={this.handleChoose} variant="outlined">
-            Choose...
-          </Button>
-        </DialogActions>
-      </Dialog>
-    );
-  }
+  return (
+    <Dialog
+      open={open}
+      onClose={onComplete}
+      disableBackdropClick
+      disableEscapeKeyDown
+    >
+      <DialogTitle>First time setup</DialogTitle>
+      <DialogContent>
+        <DialogContentText>
+          It looks like you&apos;re opening DemoMan for the first time. <br />
+          Please select the the location where your demo files are stored.
+        </DialogContentText>
+      </DialogContent>
+      <DialogActions>
+        <Button
+          onClick={() => {
+            if (GetSetDemoPath()) {
+              onComplete();
+            }
+          }}
+          variant="outlined"
+        >
+          Choose...
+        </Button>
+      </DialogActions>
+    </Dialog>
+  );
 }
