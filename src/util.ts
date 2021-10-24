5a771819cf91d7b93b1362e8318ae7663ae98c7f
Narcha <42248344+Narcha@users.noreply.github.com>
2021-10-24 13:07:35 +0200

    Improved file size formatting

--- src/util.ts
+++ src/util.ts
@@ -17,15 +17,12 @@ export function formatPlaybackTime(seconds: number): string {
 }
 
 export function formatFileSize(bytes: number): string {
-  const units = ["B", "kB", "MB", "GB"];
+  const units = ["B", "kB", "MB", "GB", "TB"];
   let size = bytes;
   let i = 0;
-  while (size > 1000) {
+  while (size >= 1000 && i < units.length - 1) {
     size /= 1000;
     i += 1;
-  }
-  if (i > 3) {
-    i = 3;
   }
   return `${size.toFixed(1)} ${units[i]}`;
 }
