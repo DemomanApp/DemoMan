d45d23085d3fbc7c74aac08e199fad267658a640
Narcha <42248344+Narcha@users.noreply.github.com>
2022-07-28 18:28:10 +0200

    Clean start

--- src/renderer/DemoEvent.ts
+++
@@ -1,5 +0,0 @@
-export default interface DemoEvent {
-  name: "Bookmark" | "Killstreak";
-  value: string;
-  tick: number;
-}
--- src/renderer/GetDemoPath.ts
+++
@@ -1,9 +0,0 @@
-import { dialog, getCurrentWindow } from "@electron/remote";
-
-export default async function getDemoPath(defaultPath?: string) {
-  return dialog.showOpenDialog(getCurrentWindow(), {
-    title: "Select your demo folder",
-    defaultPath,
-    properties: ["openDirectory", "showHiddenFiles"],
-  });
-}
