b835228ae21653dd68b0169904fafdaf1125bf3c
Narcha <42248344+Narcha@users.noreply.github.com>
2022-11-02 22:03:28 +0100

    Added AsyncCopyButton

---
+++ src/components/AsyncCopyButton.tsx
@@ -0,0 +1,36 @@
+import { writeText } from "@tauri-apps/api/clipboard";
+
+import { Tooltip, ActionIcon } from "@mantine/core";
+import { IconCheck, IconCopy } from "@tabler/icons";
+import { useAsyncCallback } from "react-async-hook";
+
+export type AsyncCopyButtonProps = {
+  text: string;
+};
+
+export default function AsyncCopyButton({ text }: AsyncCopyButtonProps) {
+  const asyncWriteText = useAsyncCallback(writeText);
+  return (
+    <Tooltip label="Copy to clipboard">
+      <ActionIcon
+        color={asyncWriteText.status == "success" ? "teal" : "gray"}
+        onClick={() =>
+          asyncWriteText.execute(text).then(() => {
+            setTimeout(() => {
+              asyncWriteText.reset();
+            }, 1000);
+
+            // eslint wants each `.then` to return something...
+            return null;
+          })
+        }
+      >
+        {asyncWriteText.status == "success" ? (
+          <IconCheck size={16} />
+        ) : (
+          <IconCopy size={16} />
+        )}
+      </ActionIcon>
+    </Tooltip>
+  );
+}
--- src/components/index.ts
+++ src/components/index.ts
@@ -1,5 +1,6 @@
 export { default as Async } from "./Async";
 export { default as AsyncButton } from "./AsyncButton";
+export { default as AsyncCopyButton } from "./AsyncCopyButton";
 export { default as ClassIcon } from "./ClassIcon";
 export { default as Fill } from "./Fill";
 export { default as KillIcon } from "./KillIcon";
