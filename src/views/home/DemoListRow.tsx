9ab21a947ef2211bba974fae49d2fe3bf3f6f503
Narcha <42248344+Narcha@users.noreply.github.com>
2022-12-14 23:37:12 +0100

    Improve demo list row looks

--- src/views/home/DemoListRow.tsx
+++ src/views/home/DemoListRow.tsx
@@ -1,12 +1,7 @@
 import { memo } from "react";
 
-import { Box, Group, Paper, Text, Title } from "@mantine/core";
-import {
-  IconCalendarEvent,
-  IconCheck,
-  IconDeviceTv,
-  IconUser,
-} from "@tabler/icons";
+import { createStyles, Group, Paper, Text, Title } from "@mantine/core";
+import { IconCalendarEvent, IconDeviceTv, IconUser } from "@tabler/icons";
 
 import { Demo, isStvDemo } from "../../demo";
 import { areEqual } from "react-window";
@@ -21,66 +16,65 @@ type DemoListRowProps = {
   onClick(): void;
 };
 
+// TODO fix the background color when hovering,
+// maybe add a hover effect to the thumbnail as well
+const useStyles = createStyles(
+  (
+    theme,
+    { selectionMode, selected }: { selectionMode: boolean; selected: boolean }
+  ) => ({
+    paper: {
+      height: "120px",
+      cursor: "pointer",
+      display: "flex",
+      alignItems: "stretch",
+      // Needed so the corners of the child elements respect the border radius
+      overflow: "hidden",
+      "&:hover": {
+        backgroundColor: selected
+          ? theme.fn.variant({ variant: "light", color: theme.primaryColor })
+              .background
+          : theme.colorScheme === "dark"
+          ? theme.colors.dark[5]
+          : theme.colors.gray[1],
+      },
+      transition: "background-color 150ms ease, border-color 150ms ease",
+      boxShadow: selected
+        ? `0px 0px 0px 2px ${
+            theme.fn.variant({ variant: "outline", color: theme.primaryColor })
+              .border
+          }`
+        : undefined,
+      borderRadius: theme.radius.md,
+      backgroundColor: selected
+        ? theme.fn.variant({ variant: "light", color: theme.primaryColor })
+            .background
+        : theme.colorScheme === "dark"
+        ? theme.colors.dark[8]
+        : theme.white,
+    },
+    content: {
+      display: "flex",
+      flexDirection: "column",
+      justifyContent: "start",
+      padding: "8px",
+      flexGrow: 1,
+    },
+  })
+);
+
 function DemoListRow({
   demo,
   selected,
   selectionMode,
   onClick,
 }: DemoListRowProps) {
+  const { classes } = useStyles({ selectionMode, selected });
   const birthtime = new Date(demo.birthtime * 1000);
   return (
-    <Paper
-      sx={(theme) => ({
-        height: "120px",
-        display: "flex",
-        alignItems: "stretch",
-        // Needed so the corners of the child elements respect the border radius
-        overflow: "hidden",
-        "&:hover": {
-          backgroundColor:
-            theme.colorScheme === "dark"
-              ? theme.colors.dark[5]
-              : theme.colors.gray[1],
-        },
-        boxShadow: selectionMode
-          ? `0 0 0 3px ${
-              selected ? theme.colors.blue[8] : theme.colors.gray[8]
-            }`
-          : "none",
-        cursor: "pointer",
-      })}
-      radius="md"
-      shadow="xl"
-      onClick={onClick}
-    >
-      {selectionMode && selected && (
-        <Box
-          sx={(theme) => ({
-            position: "absolute",
-            display: "flex",
-            justifyContent: "center",
-            alignItems: "center",
-            top: 0,
-            left: 0,
-            height: "40px",
-            width: "40px",
-            backgroundColor: theme.colors.blue[8],
-            borderBottomRightRadius: "8px",
-          })}
-        >
-          <IconCheck color="white" />
-        </Box>
-      )}
+    <Paper className={classes.paper} radius="md" shadow="xl" onClick={onClick}>
       <MapBox mapName={demo.mapName} />
-      <div
-        style={{
-          display: "flex",
-          flexDirection: "column",
-          justifyContent: "start",
-          padding: "8px",
-          flexGrow: 1,
-        }}
-      >
+      <div className={classes.content}>
         <Group spacing="xs">
           <Title order={3} inline>
             {demo.name}
