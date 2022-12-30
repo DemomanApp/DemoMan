687469cc36c6b05e2e9dd801ed8418c50aed893e
Yep <yepteamfortress2@gmail.com>
2022-12-30 13:04:15 -0500

    Update scoreboard styling
    Show scores of zero without color (same as the TF2 HUD)

--- src/views/demoDetails/PlayerList/ScoreboardTable.tsx
+++ src/views/demoDetails/PlayerList/ScoreboardTable.tsx
@@ -2,82 +2,150 @@ import React from "react";
 import { Scoreboard, EMPTY_SCOREBOARD } from "../../../demo";
 
 interface ScoreboardTableProps {
-	scoreboard: Scoreboard;
+    scoreboard: Scoreboard;
 }
 
 interface ScoreboardTableState {
-	scoreboard: Scoreboard
+    scoreboard: Scoreboard
 }
 
 export default class ScoreboardTable extends React.Component<ScoreboardTableProps, ScoreboardTableState> {
-	constructor(props: ScoreboardTableProps) {
-		super(props);
+    constructor(props: ScoreboardTableProps) {
+        super(props);
 
-		this.state = {
-			scoreboard: props.scoreboard ?? EMPTY_SCOREBOARD
-		};
-	}
+        this.state = {
+            scoreboard: props.scoreboard ?? EMPTY_SCOREBOARD
+        };
+    }
 
-	setScoreboard(newScoreboard: Scoreboard) {
-		this.setState({ scoreboard: newScoreboard ?? EMPTY_SCOREBOARD });
-	}
+    setScoreboard(newScoreboard: Scoreboard) {
+        this.setState({scoreboard: newScoreboard ?? EMPTY_SCOREBOARD});
+    }
 
-	render() {
-		const scoreboard: Scoreboard = this.state.scoreboard;
+    render() {
+        const scoreboard: Scoreboard = this.state.scoreboard;
 
-		return (
-			<div>
-				<table className="scoreboard" style={{ width: "100%" }}>
-					<thead>
-					{/* No headers */}
-					</thead>
-					<tbody>
-					<tr>
-						<td>Kills:</td>
-						<td>{ scoreboard.kills }</td>
-						<td>Captures:</td>
-						<td>{ scoreboard.captures }</td>
-						<td>Invulns:</td>
-						<td>{ scoreboard.ubercharges }</td>
-						<td>Backstabs:</td>
-						<td>{ scoreboard.backstabs }</td>
-					</tr>
+        return (
+            <div>
+                <table className={ "scoreboard" }>
+                    <thead>
+                    {/* No headers */ }
+                    </thead>
+                    <tbody>
+                    {/* Row 1: Kills|Captures|Invulns|Backstabs */ }
+                    <tr>
+                        <td className={ "score-label" }>
+                            Kills:
+                        </td>
+                        <td className={ scoreboard.kills == 0 ? "score-value zero-score" : "score-value non-zero-score" }>
+                            { scoreboard.kills }
+                        </td>
+                        <td className={ "score-label" }>
+                            Captures:
+                        </td>
+                        <td className={ scoreboard.captures == 0 ? "score-value zero-score" : "score-value non-zero-score" }>
+                            { scoreboard.captures }
+                        </td>
+                        <td className={ "score-label" }>
+                            Invulns:
+                        </td>
+                        <td className={ scoreboard.ubercharges == 0 ? "score-value zero-score" : "score-value non-zero-score" }>
+                            { scoreboard.ubercharges }
+                        </td>
+                        <td className={ "score-label" }>
+                            Backstabs:
+                        </td>
+                        <td className={ scoreboard.backstabs == 0 ? "score-value zero-score" : "score-value non-zero-score" }>
+                            { scoreboard.backstabs }
+                        </td>
+                    </tr>
 
-					<tr>
-						<td>Deaths:</td>
-						<td>{ scoreboard.deaths }</td>
-						<td>Defenses:</td>
-						<td>{ scoreboard.defenses }</td>
-						<td>Headshots:</td>
-						<td>{ scoreboard.headshots }</td>
-						<td>Bonus:</td>
-						<td>{ scoreboard.bonus_points }</td>
-					</tr>
+                    {/* Row 2: Deaths|Defenses|Headshots|Bonus */ }
+                    <tr>
+                        <td className={ "score-label" }>
+                            Deaths:
+                        </td>
+                        <td className={ scoreboard.deaths == 0 ? "score-value zero-score" : "score-value non-zero-score" }>
+                            { scoreboard.deaths }
+                        </td>
+                        <td className={ "score-label" }>
+                            Defenses:
+                        </td>
+                        <td className={ scoreboard.defenses == 0 ? "score-value zero-score" : "score-value non-zero-score" }>
+                            { scoreboard.defenses }
+                        </td>
+                        <td className={ "score-label" }>
+                            Headshots:
+                        </td>
+                        <td className={ scoreboard.headshots == 0 ? "score-value zero-score" : "score-value non-zero-score" }>
+                            { scoreboard.headshots }
+                        </td>
+                        <td className={ "score-label" }>
+                            Bonus:
+                        </td>
+                        <td className={ scoreboard.bonus_points == 0 ? "score-value zero-score" : "score-value non-zero-score" }>
+                            { scoreboard.bonus_points }
+                        </td>
+                    </tr>
 
-					<tr>
-						<td>Assists:</td>
-						<td>{ scoreboard.assists }</td>
-						<td>Domination:</td>
-						<td>{ scoreboard.dominations }</td>
-						<td>Teleports:</td>
-						<td>{ scoreboard.teleports }</td>
-						<td>Support:</td>
-						<td>{ scoreboard.support }</td>
-					</tr>
+                    {/* Row 3: Assists|Domination|Teleports|Support */ }
+                    <tr>
+                        <td className={ "score-label" }>
+                            Assists:
+                        </td>
+                        <td className={ scoreboard.assists == 0 ? "score-value zero-score" : "score-value non-zero-score" }>
+                            { scoreboard.assists }
+                        </td>
+                        <td className={ "score-label" }>
+                            Domination:
+                        </td>
+                        <td className={ scoreboard.dominations == 0 ? "score-value zero-score" : "score-value non-zero-score" }>
+                            { scoreboard.dominations }
+                        </td>
+                        <td className={ "score-label" }>
+                            Teleports:
+                        </td>
+                        <td className={ scoreboard.teleports == 0 ? "score-value zero-score" : "score-value non-zero-score" }>
+                            { scoreboard.teleports }
+                        </td>
+                        <td className={ "score-label" }>
+                            Support:
+                        </td>
+                        <td className={ scoreboard.support == 0 ? "score-value zero-score" : "score-value non-zero-score" }>
+                            { scoreboard.support }
+                        </td>
+                    </tr>
 
-					<tr>
-						<td>Destruction:</td>
-						<td>{ scoreboard.buildings_destroyed }</td>
-						<td>Revenge:</td>
-						<td>{ scoreboard.revenges }</td>
-						<td>Healing:</td>
-						<td>{ scoreboard.healing }</td>
-						<td>Damage:</td>
-						<td>{ scoreboard.damage_dealt }</td>
-					</tr>
-					</tbody>
-				</table>
-			</div>
-		);
-	}
+                    {/* Row 4: Destruction|Revenge|Healing|Damage */ }
+                    <tr>
+                        <td className={ "score-label" }>
+                            Destruction:
+                        </td>
+                        <td className={ scoreboard.buildings_destroyed == 0 ? "score-value zero-score" : "score-value non-zero-score" }>
+                            { scoreboard.buildings_destroyed }
+                        </td>
+                        <td className={ "score-label" }>
+                            Revenge:
+                        </td>
+                        <td className={ scoreboard.revenges == 0 ? "score-value zero-score" : "score-value non-zero-score" }>
+                            { scoreboard.revenges }
+                        </td>
+                        <td className={ "score-label" }>
+                            Healing:
+                        </td>
+                        <td className={ scoreboard.healing == 0 ? "score-value zero-score" : "score-value non-zero-score" }>
+                            { scoreboard.healing }
+                        </td>
+                        <td className={ "score-label" }>
+                            Damage:
+                        </td>
+                        <td className={ scoreboard.damage_dealt == 0 ? "score-value zero-score" : "score-value non-zero-score" }>
+                            { scoreboard.damage_dealt }
+                        </td>
+                    </tr>
+                    </tbody>
+                </table>
+            </div>
+        );
+    }
 }
