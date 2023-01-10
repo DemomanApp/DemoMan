2358ed57758a4f0003e1b4737844a2e9fc7ffbfe
Yep <yepteamfortress2@gmail.com>
2023-01-10 18:30:31 -0500

    Correct method signature for getDefaultDemosDir

--- src/views/settings/storage.ts
+++ src/views/settings/storage.ts
@@ -5,7 +5,7 @@ import { getTf2Dir } from "../../api";
  * Gets the default platform-specific demo file storage directory.
  * Only supports Windows, Mac, and Linux.  This will return `null` if run on any other platform.
  */
-export async function getDefaultDemosDir(): Promise<string | null> {
+export async function getDefaultDemosDir(): Promise<string> {
   const userHomeDir = await homeDir();
 
   // Try getting the TF2 game directory, falling back to the user home directory if it couldn't be found
