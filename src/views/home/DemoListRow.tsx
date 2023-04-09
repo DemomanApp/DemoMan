1da3d2cfe56b5855263b309e047f0b5da44c3390
Narcha <42248344+Narcha@users.noreply.github.com>
2023-04-09 18:31:42 +0200

    Add hover menu to DemoListRow, replace event box

--- src/views/home/DemoListRow.tsx
+++ src/views/home/DemoListRow.tsx
@@ -1,11 +1,33 @@
 import { memo } from "react";
 
-import { createStyles, Group, Paper, Text, Title } from "@mantine/core";
-import { IconCalendarEvent, IconDeviceTv, IconUser } from "@tabler/icons-react";
+import {
+  ActionIcon,
+  createStyles,
+  getStylesRef,
+  Group,
+  HoverCard,
+  Paper,
+  ScrollArea,
+  Stack,
+  Text,
+  Title,
+  Tooltip,
+} from "@mantine/core";
+import {
+  Icon as IconType,
+  IconCalendarEvent,
+  IconDeviceTv,
+  IconPencil,
+  IconTrash,
+  IconUser,
+  IconPlayerPlayFilled,
+  IconBookmarks,
+  IconBookmark,
+} from "@tabler/icons-react";
+import { areEqual } from "react-window";
 
 import { Demo, isStvDemo } from "../../demo";
-import { areEqual } from "react-window";
-import EventsBox from "./EventsBox";
+import { IconKillstreak } from "../../components/icons";
 import MapBox from "./MapBox";
 import Badges from "./Badges";
 
@@ -16,12 +38,38 @@ type DemoListRowProps = {
   onClick(): void;
 };
 
+function HoverMenuItem({
+  Icon,
+  label,
+  onClick,
+}: {
+  Icon: IconType;
+  label: string;
+  onClick(): void;
+}) {
+  return (
+    <Tooltip label={label}>
+      <ActionIcon
+        onClick={(event) => {
+          onClick();
+          event.stopPropagation();
+        }}
+      >
+        <Icon size={18} />
+      </ActionIcon>
+    </Tooltip>
+  );
+}
+
 // TODO fix the background color when hovering,
 // maybe add a hover effect to the thumbnail as well
 const useStyles = createStyles(
   (
     theme,
-    { selectionMode: _, selected }: { selectionMode: boolean; selected: boolean }
+    {
+      selectionMode: _,
+      selected,
+    }: { selectionMode: boolean; selected: boolean }
   ) => ({
     paper: {
       height: "120px",
@@ -37,6 +85,9 @@ const useStyles = createStyles(
           : theme.colorScheme === "dark"
           ? theme.colors.dark[5]
           : theme.colors.gray[1],
+        [`& .${getStylesRef("menu")}`]: {
+          display: "flex",
+        },
       },
       transition: "background-color 150ms ease, border-color 150ms ease",
       boxShadow: selected
@@ -57,8 +108,18 @@ const useStyles = createStyles(
       display: "flex",
       flexDirection: "column",
       justifyContent: "start",
+      alignItems: "start",
       padding: "8px",
       flexGrow: 1,
+    },
+    menu: {
+      ref: getStylesRef("menu"),
+      display: "none", // Changes to "flex" when hovered
+      alignItems: "center",
+      position: "absolute",
+      top: -16,
+      right: 16,
+      height: 32,
     },
   })
 );
@@ -92,8 +153,85 @@ function DemoListRow({
           <IconCalendarEvent color="gray" />
           <Text color="dimmed">{birthtime.toLocaleString()}</Text>
         </Group>
+        {demo.events.length !== 0 && (
+          <HoverCard
+            withArrow
+            position="right-end"
+            styles={(theme) => ({
+              dropdown: {
+                padding: 0,
+                paddingLeft: theme.spacing.xs,
+              },
+            })}
+          >
+            <HoverCard.Target>
+              <Group spacing={4}>
+                <IconBookmarks color="gray" />
+                <Text color="dimmed">{demo.events.length} Bookmarks</Text>
+              </Group>
+            </HoverCard.Target>
+            <HoverCard.Dropdown>
+              <ScrollArea.Autosize
+                mah="10rem"
+                offsetScrollbars
+                type="auto"
+                styles={(theme) => ({
+                  viewport: {
+                    paddingTop: theme.spacing.xs,
+                    paddingBottom: theme.spacing.xs,
+                  },
+                })}
+              >
+                <Stack align="stretch" spacing={0}>
+                  {demo.events.map((event, idx) => (
+                    <Group key={idx} align="center" position="left" spacing={0}>
+                      {event.name === "Bookmark" ? (
+                        <IconBookmark />
+                      ) : (
+                        <IconKillstreak />
+                      )}
+                      <Text
+                        sx={(theme) => ({
+                          flexGrow: 1,
+                          marginRight: theme.spacing.xs,
+                        })}
+                      >
+                        {event.value}
+                      </Text>
+                      <Text color="dimmed" size="xs">
+                        {event.tick}
+                      </Text>
+                    </Group>
+                  ))}
+                </Stack>
+              </ScrollArea.Autosize>
+            </HoverCard.Dropdown>
+          </HoverCard>
+        )}
       </div>
-      <EventsBox events={demo.events} />
+      <Paper shadow="xl" withBorder className={classes.menu}>
+        <HoverMenuItem
+          Icon={IconTrash}
+          label="Delete"
+          onClick={() => {
+            console.log("delete");
+          }}
+        />
+        <HoverMenuItem
+          Icon={IconPencil}
+          label="Rename"
+          onClick={() => {
+            console.log("rename");
+          }}
+        />
+        <HoverMenuItem
+          Icon={IconPlayerPlayFilled}
+          label="Play"
+          onClick={() => {
+            console.log("play");
+          }}
+        />
+      </Paper>
     </Paper>
   );
 }
