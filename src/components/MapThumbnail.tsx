3eb8c0111c1350ce0b878e099819065dabb0a910
Narcha <42248344+Narcha@users.noreply.github.com>
2023-05-04 17:14:02 +0200

    Refactor map thumbnails

--- src/components/MapThumbnail.tsx
+++ src/components/MapThumbnail.tsx
@@ -1,36 +1,28 @@
-import { CSSProperties, useState } from "react";
 import { normalizeMapName } from "../util";
+
+const MAP_THUMBNAILS = import.meta.glob("../assets/map_thumbnails/*.png", {
+  as: "url",
+  eager: true,
+});
+
+function getThumbnail(mapName: string) {
+  return MAP_THUMBNAILS[`../assets/map_thumbnails/${mapName}.png`];
+}
 
 export type MapThumbnailProps = {
   mapName: string;
-  style?: CSSProperties;
   className?: string;
   fallback: JSX.Element;
 };
 
 export default function MapThumbnail({
   mapName,
-  style,
   className,
   fallback,
 }: MapThumbnailProps) {
-  const [loadFailed, setLoadFailed] = useState(false);
-  const [loadSucceeded, setLoadSucceeded] = useState(false);
-
-  if (loadFailed) {
-    return (
-      <div style={style} className={className}>
-        {fallback}
-      </div>
-    );
+  const thumbnail = getThumbnail(normalizeMapName(mapName));
+  if (thumbnail === undefined) {
+    return <div className={className}>{fallback}</div>;
   }
-  return (
-    <img
-      src={`/map_thumbnails/${normalizeMapName(mapName)}.png`}
-      onError={() => setLoadFailed(true)}
-      onLoad={() => setLoadSucceeded(true)}
-      style={{ opacity: loadSucceeded ? 1 : 0, ...style }}
-      className={className}
-    />
-  );
+  return <img src={thumbnail} className={className} />;
 }
