9686798f904d2f69dcdd13c5e87be2da6e7ce584
Narcha <42248344+Narcha@users.noreply.github.com>
2023-05-05 15:29:45 +0200

    Tweak settings page

--- src/views/settings/index.tsx
+++ src/views/settings/index.tsx
@@ -1,25 +1,33 @@
 import useStore from "../../hooks/useStore";
-import { Button, createStyles, ScrollArea, TextInput } from "@mantine/core";
+import {
+  Button,
+  createStyles,
+  ScrollArea,
+  Text,
+  TextInput,
+} from "@mantine/core";
 import { open as dialogOpen } from "@tauri-apps/api/dialog";
+import { HeaderPortal } from "../../AppShell";
 
-const useStyles = createStyles({
+const useStyles = createStyles((theme) => ({
   root: {
-    margin: "12px",
-    display: "flex",
-    flexDirection: "column",
-    gap: "8px",
-    height: "100%"
+    height: "100%",
   },
   row: {
     display: "flex",
-    flexDirection: "row",
-    gap: "8px",
+    gap: theme.spacing.xs,
   },
   rowLabel: {
     width: 160,
     fontSize: "16pt",
   },
-});
+  viewport: {
+    display: "flex",
+    flexDirection: "column",
+    gap: theme.spacing.sm,
+    margin: theme.spacing.sm,
+  },
+}));
 
 export default function SettingsView() {
   const [demoPath, setDemoPath] = useStore("demoPath");
@@ -27,36 +35,48 @@ export default function SettingsView() {
   const { classes } = useStyles();
 
   return (
-    <div className={classes.root}>
-      <h1>Settings</h1>
-
-      <ScrollArea>
-        <span className={classes.row}>
-          <span className={classes.rowLabel}>
-            <label>Demos Folder</label>
-          </span>
-          <Button
-            onClick={() => {
-              dialogOpen({
-                directory: true,
-                defaultPath: demoPath,
-                title: "Select Demo Storage Folder",
-              })
-                .then((value) => {
-                  if (value !== null && value !== "") {
-                    // Don't set the path if the user cancelled the dialog
-                    setDemoPath(value as string);
-                  }
-                  return;
+    <>
+      <HeaderPortal>
+        <Text
+          align="center"
+          weight={500}
+          size="lg"
+          inline
+          style={{ cursor: "default" }}
+          data-tauri-drag-region
+        >
+          Settings
+        </Text>
+      </HeaderPortal>
+      <ScrollArea classNames={{ root: classes.root }}>
+        <div className={classes.viewport}>
+          <span className={classes.row}>
+            <span className={classes.rowLabel}>
+              <label>Demos Folder</label>
+            </span>
+            <Button
+              onClick={() => {
+                dialogOpen({
+                  directory: true,
+                  defaultPath: demoPath,
+                  title: "Select Demo Storage Folder",
                 })
-                .catch((error) => console.error(error));
-            }}
-          >
-            Select a folder...
-          </Button>
-          <TextInput value={demoPath ?? ""} disabled={true}></TextInput>
-        </span>
+                  .then((value) => {
+                    if (value !== null && value !== "") {
+                      // Don't set the path if the user cancelled the dialog
+                      setDemoPath(value as string);
+                    }
+                    return;
+                  })
+                  .catch((error) => console.error(error));
+              }}
+            >
+              Select a folder...
+            </Button>
+            <TextInput value={demoPath ?? ""} disabled={true}></TextInput>
+          </span>
+        </div>
       </ScrollArea>
-    </div>
+    </>
   );
 }
