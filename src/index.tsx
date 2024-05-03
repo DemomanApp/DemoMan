417f114ba208e486e8922f7210c23353bdbe00cc
Narcha <42248344+Narcha@users.noreply.github.com>
2024-05-03 20:54:41 +0200

    Remove focus ring

--- src/index.tsx
+++ src/index.tsx
@@ -20,6 +20,7 @@ createRoot(document.getElementById("root")!).render(
     <MantineProvider
       defaultColorScheme="dark"
       theme={{
+        focusRing: "never",
         components: {
           Tooltip: {
             defaultProps: {
