4ab5890b156fd7ace711a997ecc557510dc4a1c0
Rasmus Mecklenburg <42248344+rmburg@users.noreply.github.com>
2024-11-28 01:16:10 +0100

    Serialize error as string

--- src-tauri/src/demo/error.rs
+++ src-tauri/src/demo/error.rs
@@ -1,62 +1,29 @@
-use std::{fmt::Display, io};
-
 use serde::{Serialize, Serializer};
 
 use crate::disk_cache;
 
 pub type Result<T, E = Error> = std::result::Result<T, E>;
 
-fn serialize_as_string<T: Display, S>(value: &T, serializer: S) -> Result<S::Ok, S::Error>
-where
-    S: Serializer,
-{
-    serializer.serialize_str(value.to_string().as_str())
-}
-
-#[derive(Debug, Serialize, thiserror::Error)]
+#[derive(Debug, thiserror::Error)]
 pub enum Error {
     #[error("bad filename")]
     BadFilename,
     #[error("bincode (de-)serialization failed: {0}")]
-    Bincode(
-        #[from]
-        #[serde(serialize_with = "serialize_as_string")]
-        bincode::Error,
-    ),
+    Bincode(#[from] bincode::Error),
     #[error("bitbuffer error: {0}")]
-    Bitbuffer(
-        #[from]
-        #[serde(serialize_with = "serialize_as_string")]
-        bitbuffer::BitError,
-    ),
+    Bitbuffer(#[from] bitbuffer::BitError),
     #[error("failed to trash file: {0}")]
-    Trash(
-        #[from]
-        #[serde(serialize_with = "serialize_as_string")]
-        trash::Error,
-    ),
+    Trash(#[from] trash::Error),
     #[error("not a file")]
     NotAFile,
     #[error("not TF2")]
     NotTf2,
     #[error("I/O error: {0}")]
-    Io(
-        #[from]
-        #[serde(serialize_with = "serialize_as_string")]
-        io::Error,
-    ),
+    Io(#[from] std::io::Error),
     #[error("parsing failed")]
-    DemoParsing(
-        #[from]
-        #[serde(serialize_with = "serialize_as_string")]
-        tf_demo_parser::ParseError,
-    ),
+    DemoParsing(#[from] tf_demo_parser::ParseError),
     #[error("JSON (de-)serialization failed: {0}")]
-    SerdeJson(
-        #[from]
-        #[serde(serialize_with = "serialize_as_string")]
-        serde_json::Error,
-    ),
+    SerdeJson(#[from] serde_json::Error),
 }
 
 impl From<disk_cache::Error> for Error {
@@ -66,5 +33,14 @@ impl From<disk_cache::Error> for Error {
             disk_cache::Error::Serde(bincode_error) => Self::from(bincode_error),
             disk_cache::Error::InvalidFilename(_) => Self::BadFilename,
         }
+    }
+}
+
+impl Serialize for Error {
+    fn serialize<S>(&self, serializer: S) -> std::result::Result<S::Ok, S::Error>
+    where
+        S: Serializer,
+    {
+        serializer.serialize_str(self.to_string().as_str())
     }
 }
