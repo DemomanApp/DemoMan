80711390cd64dc6f985bcfba1a2830eb0b7e5358
Rasmus Mecklenburg <42248344+rmburg@users.noreply.github.com>
2025-01-23 00:07:04 +0100

    Fix title in RCON console

--- src/routes/rconConsole/index.tsx
+++ src/routes/rconConsole/index.tsx
@@ -91,7 +91,7 @@ export default function RconConsole() {
                 cursor: "default",
               }}
             >
-              Settings
+              RCON Console
             </Text>
           }
         />
