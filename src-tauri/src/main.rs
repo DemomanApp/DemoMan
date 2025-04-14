fdae93a4ef7ceb5a287e808367863ebb0c2653e8
Narcha <42248344+rmburg@users.noreply.github.com>
2025-04-14 13:15:50 +0200

    Add tauri-plugin-opener dependency

--- src-tauri/src/main.rs
+++ src-tauri/src/main.rs
@@ -68,6 +68,7 @@ fn main() {
     let args = Args::parse();
 
     tauri::Builder::default()
+        .plugin(tauri_plugin_opener::init())
         .plugin(tauri_plugin_shell::init())
         .plugin(tauri_plugin_dialog::init())
         .plugin(tauri_plugin_clipboard_manager::init())
