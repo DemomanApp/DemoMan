a568356caa1d9920a696234cbad42456264eb272
Narcha <42248344+rmburg@users.noreply.github.com>
2025-04-17 16:39:27 +0200

    Fix lint/style/useNodejsImportProtocol lints

--- vite.config.ts
+++ vite.config.ts
@@ -1,6 +1,6 @@
 import { defineConfig } from "vite";
 import react from "@vitejs/plugin-react";
-import path from "path";
+import path from "node:path";
 
 const host = process.env.TAURI_DEV_HOST;
 
