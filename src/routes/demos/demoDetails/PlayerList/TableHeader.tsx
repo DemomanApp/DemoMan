c0d8145277e6646d621ba6a52c9e2c5a7da30484
Narcha <42248344+rmburg@users.noreply.github.com>
2025-04-17 12:57:19 +0200

    Fix lint/style/useSelfClosingElements lints

--- src/routes/demos/demoDetails/PlayerList/TableHeader.tsx
+++ src/routes/demos/demoDetails/PlayerList/TableHeader.tsx
@@ -3,7 +3,7 @@ import classes from "./TableHeader.module.css";
 export function TableHeader() {
   return (
     <div className={classes.root}>
-      <span className={classes.smallColumn}></span>
+      <span className={classes.smallColumn} />
       <span className={classes.playerColumn}>Name</span>
       <span className={classes.scoreColumn}>Score</span>
       <span className={classes.smallColumn}>K</span>
