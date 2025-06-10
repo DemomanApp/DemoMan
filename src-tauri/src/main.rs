b44a4b72cdf409dca069a61db16a90ffc8504e37
rmburg <42248344+rmburg@users.noreply.github.com>
2025-06-11 00:18:26 +0200

    Add updater plugin

--- src-tauri/src/main.rs
+++ src-tauri/src/main.rs
@@ -77,6 +77,7 @@ fn main() {
     let args = Args::parse();
 
     tauri::Builder::default()
+        .plugin(tauri_plugin_updater::Builder::new().build())
         .plugin(tauri_plugin_opener::init())
         .plugin(tauri_plugin_shell::init())
         .plugin(tauri_plugin_dialog::init())
