777cc45fb5f16ceaebdb9c359e61f1004813f69a
rmburg <42248344+rmburg@users.noreply.github.com>
2025-06-27 13:51:33 +0200

    Add button to add demo directory in-place

---
+++ src/modals/DemoDirModal.tsx
@@ -0,0 +1,85 @@
+import { Button, Group, Input, TextInput } from "@mantine/core";
+import { type ContextModalProps, modals } from "@mantine/modals";
+import { useForm } from "@mantine/form";
+import useStore from "@/hooks/useStore";
+import PathPicker from "@/routes/settings/PathPicker";
+
+export function openAddDemoDirModal() {
+  modals.openContextModal({
+    modal: "add_demo_dir",
+    title: "Add demo directory",
+    centered: true,
+    size: "lg",
+    innerProps: {},
+  });
+}
+
+export const AddDemoDirModal = ({ id }: ContextModalProps) => {
+  const [demoDirs, setDemoDirs] = useStore("demoDirs");
+
+  const form = useForm<{ label: string; path: string }>({
+    initialValues: {
+      label: "My Demos",
+      path: "",
+    },
+    validate: {
+      label: (newLabel) => {
+        if (newLabel.length === 0) {
+          return "Label cannot be empty";
+        }
+        if (Object.values(demoDirs).some((label) => label === newLabel)) {
+          return "A demo directory with this label already exists";
+        }
+        return null;
+      },
+      path: (newPath: string) => {
+        if (newPath.length === 0) {
+          return "Path cannot be empty";
+        }
+        if (Object.keys(demoDirs).some((path) => path === newPath)) {
+          return "A demo directory with this path already exists";
+        }
+        return null;
+      },
+    },
+    validateInputOnBlur: true,
+  });
+
+  return (
+    <form
+      onSubmit={form.onSubmit(({ label, path }) => {
+        setDemoDirs((previousDemoDirs) => ({
+          ...previousDemoDirs,
+          [path]: label,
+        }));
+        modals.close(id);
+      })}
+    >
+      <TextInput
+        label="Label"
+        description="Give this directory a short name"
+        {...form.getInputProps("label")}
+      />
+      <Input.Wrapper
+        label="Path"
+        description="The file path to the directory"
+        mt="md"
+        {...form.getInputProps("path")}
+      >
+        <Input
+          component={PathPicker}
+          styles={{
+            wrapper: {
+              flexGrow: 1,
+            },
+          }}
+          setValue={(newPath) => form.setFieldValue("path", newPath)}
+          {...form.getInputProps("path")}
+        />
+      </Input.Wrapper>
+      <Group justify="flex-end" mt="md">
+        <Button type="submit">Submit</Button>
+      </Group>
+    </form>
+  );
+};
--- src/routes/demos/index.tsx
+++ src/routes/demos/index.tsx
@@ -1,11 +1,13 @@
 import { Link } from "react-router";
 
-import { Anchor, Stack, Text, Title } from "@mantine/core";
+import { Anchor, Button, Stack, Text, Title } from "@mantine/core";
+import { IconPlus } from "@tabler/icons-react";
 
 import { HeaderPortal } from "@/AppShell";
 import { Fill } from "@/components";
 import useStore from "@/hooks/useStore";
 import DemoDirButton from "./DemoDirButton";
+import { openAddDemoDirModal } from "@/modals/DemoDirModal";
 
 export default () => {
   const [demoDirs] = useStore("demoDirs");
@@ -15,25 +17,30 @@ export default () => {
     <>
       <HeaderPortal />
       <Fill>
-        <Stack gap="sm">
+        <Stack gap="md" align="center">
           <Title order={2} ta="center">
             Select a demo directory
           </Title>
           {demoDirEntries.length === 0 ? (
             <Text ta="center">No demo directories set.</Text>
           ) : (
-            demoDirEntries.map(([path, label]) => (
-              <DemoDirButton path={path} label={label} key={path} />
-            ))
+            <Stack gap="sm" style={{ alignSelf: "stretch" }}>
+              {demoDirEntries.map(([path, label]) => (
+                <DemoDirButton path={path} label={label} key={path} />
+              ))}
+            </Stack>
           )}
 
-          <Text ta="center">
-            {/*
-                TODO: Maybe allow demo directories to be added in-place?
-                      Or maybe add functionality to link to a specific setting
-                      and have it highlighted         
-              */}
-            You can add or remove demo directories in the{" "}
+          <Button
+            leftSection={<IconPlus />}
+            variant="subtle"
+            onClick={openAddDemoDirModal}
+          >
+            Add demo directory
+          </Button>
+
+          <Text c="dimmed">
+            You can manage your demo directories in the{" "}
             <Anchor component={Link} to="/settings">
               settings
             </Anchor>
