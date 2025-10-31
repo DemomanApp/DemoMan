628bb2736ca8fadaa63e15e745465d4929e45902
rmburg <42248344+rmburg@users.noreply.github.com>
2025-10-31 19:58:14 +0100

    Add validation to local storage
    previously, values set by one version of DemoMan being invalid in a future version caused a runtime error

--- src/hooks/useStore.ts
+++ src/hooks/useStore.ts
@@ -1,11 +1,13 @@
 import { useLocalStorage } from "@mantine/hooks";
 
-import { type StoreSchema, storeDefault } from "@/store";
+import { type StoreSchema, storeDefault, validateStoreValue } from "@/store";
 
 export default function useStore<K extends keyof StoreSchema>(key: K) {
-  return useLocalStorage<StoreSchema[K]>({
+  const defaultValue = storeDefault(key);
+
+  const [value, setValue] = useLocalStorage<StoreSchema[K]>({
     key,
-    defaultValue: storeDefault(key),
+    defaultValue,
     // Force the hook to load the value on the first render,
     // instead of loading the default first and then
     // loading the store value in a useEffect hook later.
@@ -13,4 +15,11 @@ export default function useStore<K extends keyof StoreSchema>(key: K) {
     // but prevents an invalid intermediate state.
     getInitialValueInEffect: false,
   });
+
+  if (validateStoreValue(key, value)) {
+    return [value, setValue] as const;
+  } else {
+    setValue(defaultValue);
+    return [defaultValue, setValue] as const;
+  }
 }
